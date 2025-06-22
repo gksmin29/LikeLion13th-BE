474d349009295c7ef666fee431735f3a083eea23
hanmin <rlemdhs2747@naver.com>
2025-06-22 20:23:18 +0900

    ci: cicd 수정4

--- src/main/java/com/project/likelion13thbe/global/config/SwaggerConfig.java
+++ src/main/java/com/project/likelion13thbe/global/config/SwaggerConfig.java
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-    // http://localhost:8080/swagger-ui/index.html#/
+    //http://localhost:8080/swagger-ui/index.html#/
 
 
     @Bean
